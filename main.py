52e630be1c8cabad15ad7da2629fb57819178810
BackTracK <78544420+maplexx14@users.noreply.github.com>
2023-09-12 01:08:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ media = 'media/'
 ytdlOps = {
     'format': 'mp3/bestaudio/best',
     'outtmpl': 'media/%(id)s.%(ext)s',
-    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-    'postprocessors': [{  # Extract audio using ffmpeg
+   
+    'postprocessors': [{  
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
     }]
@@ -36,12 +36,11 @@ async def on_ready():
 async def play(ctx, *, link):
     print(ctx.message.author.name + "#" + ctx.message.author.discriminator + " played some music. " + str(link))
 
-    # get the right voice connection
+ 
     voiceChannel = audioTools.getVoiceChannel(ctx, bot)
     print("Gonna try to play some music")
     print(bot.voice_clients)
 
-    # check for invalid input to prevent data injection
     if audioTools.checkInvalidLink(link):
         await ctx.send(
             embed=discord.Embed(title="Ошибка", description="Введите YT ID(.\nПример: `!play dQw4w9WgXcQ`",
@@ -49,14 +48,13 @@ async def play(ctx, *, link):
         await ctx.send(file=discord.File('1.png'))
         return
 
-    # if the bot isn't connected to a voice channel, then voiceChannel = -1
     if voiceChannel == -1:
         await ctx.send(
             embed=discord.Embed(title="Ошибка", description="Присоедини бота к каналу. `!join`",
                                 color=0x00BBFF))
         return
 
-    # if the file does exist, play!
+ 
     if os.path.isfile(media + link + ".mp3"):
         try:
             await ctx.send(embed=discord.Embed(title='Сейчас играет', description=f'Поставил **{ctx.message.author.name}**',
@@ -67,13 +65,12 @@ async def play(ctx, *, link):
                                                color=0x00BBFF))
         return
 
-    # show typing while we download the file so the user knows something is happenning.
     async with ctx.typing():
-        # the file doesn't exist! Need to download it.
+     
         with yt_dlp.YoutubeDL(ytdlOps) as ydl:
             error_code = ydl.download([link])
     await ctx.send(embed=discord.Embed(title='Сейчас играет', description=f'Поставил **{ctx.message.author.name}**', color=0x00BBFF))
-    # play the audio
+
     voiceChannel.play(discord.FFmpegPCMAudio(media + link + ".mp3"))
 
 
@@ -81,9 +78,9 @@ async def play(ctx, *, link):
 @bot.command()
 
 async def pause(ctx):
-    # get voice channel
+
     voiceChannel = audioTools.getVoiceChannel(ctx, bot)
-    # if the bot isn't connected to a voice channel, then voiceChannel = -1
+
     if voiceChannel == -1:
         await ctx.send(embed = discord.Embed(title = "Ошибка", description = "Ты не в войс чате", color = 0x00BBFF))
         return
@@ -92,21 +89,21 @@ async def pause(ctx):
 
 @bot.command()
 async def resume(ctx):
-    # get voice channel
+
     voiceChannel = audioTools.getVoiceChannel(ctx, bot)
-    # if the bot isn't connected to a voice channel, then voiceChannel = -1
+
     if voiceChannel == -1:
         await ctx.send(embed = discord.Embed(title = "Ошибка", description = "Не в войсе", color = 0x880000))
         return
     voiceChannel.resume()
 
 
-# stop playing audio
+
 @bot.command()
 async def skip(ctx):
-    # get voice channel
+ 
     voiceChannel = audioTools.getVoiceChannel(ctx, bot)
-    # if the bot isn't connected to a voice channel, then voiceChannel = -1
+
     if voiceChannel == -1:
         await ctx.send(embed = discord.Embed(title = "Ошибка", description = "Не в войсе", color = 0x880000))
         return
