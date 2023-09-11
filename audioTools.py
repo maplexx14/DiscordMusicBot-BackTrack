34e55daef82aab3383ee85be7fd27c0c83d7f8ed
BackTracK <78544420+maplexx14@users.noreply.github.com>
2023-09-12 01:10:08 +0300

    Update audioTools.py

--- audioTools.py
+++ audioTools.py
@@ -3,18 +3,18 @@ import re  # regex
 
 def preJoinCheck(ctx):
     if str(ctx.message.author.voice) == "None":
-        # if the user is not in a voice channel?
+     
         return 0
-    # otherwise, return the voice channel
+ 
     return ctx.message.author.voice.channel
 
 
 def getVoiceChannel(ctx, bot):
     if bot.voice_clients == []:
         return -1
-    # get the right voice connection
+  
     voiceChannel = bot.voice_clients[0]
-    # print(bot.voice_clients)
+ 
     for connections in bot.voice_clients:
         if connections.guild == ctx.guild:
             voiceChannel = connections
@@ -23,14 +23,13 @@ def getVoiceChannel(ctx, bot):
 
 
 def checkInvalidLink(link):
-    # finds the second spacebar in message
+    
     a = link.find(" ")
     if a != -1:
         link = link[:a]
-        # delete all text after the spacebar
-
-    # Check for attack/illegal characters in link.
-    if not re.search("^[a-zA-Z0-9_-]{11}$", link):  # regex expression for a valid youtube video id.
+       
+  
+    if not re.search("^[a-zA-Z0-9_-]{11}$", link):  
         return True
 
     return False
